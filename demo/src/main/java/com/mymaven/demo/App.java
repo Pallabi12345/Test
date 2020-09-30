e6bdd5bb09a55fbba6af9b6a3ffe01b21677f405
Pallabi123 <palwork023@gmail.com>
2020-09-30 23:40:55 +0530

    DateUtils Dependency added

--- demo/src/main/java/com/mymaven/demo/App.java
+++ demo/src/main/java/com/mymaven/demo/App.java
@@ -1,5 +1,10 @@
 package com.mymaven.demo;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateUtils;
+
 /**
  * Hello world!
  *
@@ -10,6 +15,23 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hello again");
+        
+        Calendar c1=Calendar.getInstance();
+		Calendar c2=Calendar.getInstance();
+		boolean b=DateUtils.isSameDay(c1, c2);
+		if(b==true)
+			System.out.println("equal date ");
+		else
+			System.out.println("not equal date");
+		Date d1=new Date(562312L);
+		Date d2=new Date(121123456L);
+		b=DateUtils.isSameDay(d1, d2);
+		if(b==true)
+			System.out.println("same date");
+		else
+			System.out.println("not same");
+		
+
         
     }
 }
